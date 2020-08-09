65298429cb38a4a717517ecb4d1f6af9216092ad
Tyson Andre <tysonandre775@hotmail.com>
2020-08-09 13:45:46 -0400

    Add script to dump missing classes/interfaces

--- check_missing_entries.php
+++ check_missing_entries.php
@@ -1,30 +1,88 @@
 <?php
 
-$contents = file_get_contents('php.vim');
-$token_set = array_flip(array_map('strtolower', preg_split('/\s+/', $contents)));
-$extensions = get_loaded_extensions();
-sort($extensions);
-foreach ($extensions as $extension) {
-    $functions = get_extension_funcs($extension);
-    if (!$functions) {
-        continue;
-    }
-    sort($functions);
-    $missing = [];
-    $found = [];
-    foreach ($functions ?: [] as $function_name) {
-        if (isset($token_set[strtolower($function_name)])) {
-            $found[] = $function_name;
+function dump_missing_functions() {
+    echo <<<EOT
+================================================================================
+Missing functions
+================================================================================
+
+EOT;
+    $contents = file_get_contents('php.vim');
+    $token_set = array_flip(array_map('strtolower', preg_split('/\s+/', $contents)));
+    $extensions = get_loaded_extensions();
+    sort($extensions);
+    foreach ($extensions as $extension) {
+        $functions = get_extension_funcs($extension);
+        if (!$functions) {
             continue;
         }
-        $missing[] = $function_name;
-    }
-    if ($missing) {
-        echo "For extension $extension\n";
-        if ($found) {
-            echo "Exists:  " . implode(' ', $found) . "\n";
+        sort($functions);
+        $missing = [];
+        $found = [];
+        foreach ($functions ?: [] as $function_name) {
+            if (isset($token_set[strtolower($function_name)])) {
+                $found[] = $function_name;
+                continue;
+            }
+            $missing[] = $function_name;
         }
-        echo "Missing: " . implode(' ', $missing) . "\n";
-        echo "\n";
+        if ($missing) {
+            echo "For extension $extension\n";
+            if ($found) {
+                echo "Exists:  " . implode(' ', $found) . "\n";
+            }
+            echo "Missing: " . implode(' ', $missing) . "\n";
+            echo "\n";
+        }
     }
+    echo "\n\n";
 }
+
+function dump_missing_classes() {
+    echo <<<EOT
+================================================================================
+Missing classes
+================================================================================
+
+EOT;
+    $contents = file_get_contents('php.vim');
+    $token_set = array_flip(array_map('strtolower', preg_split('/\s+/', $contents)));
+    $extensions = get_loaded_extensions();
+    sort($extensions);
+    foreach ($extensions as $extension) {
+        $class_names = (new ReflectionExtension($extension))->getClassNames();
+        if (!$class_names) {
+            continue;
+        }
+        sort($class_names);
+        $missing = [];
+        $found = [];
+        foreach ($class_names ?: [] as $class_name) {
+            if (isset($token_set[strtolower($class_name)])) {
+                $found[] = $class_name;
+                continue;
+            }
+            if (class_exists($class_name)) {
+                $type = 'class';
+            } elseif (interface_exists($class_name)) {
+                $type = 'interface';
+            } else {
+                $type = 'trait';
+            }
+            $missing[$type][] = $class_name;
+        }
+        if ($missing) {
+            echo "For extension $extension\n";
+            if ($found) {
+                echo "Exists:  " . implode(' ', $found) . "\n";
+            }
+            foreach ($missing as $type => $missing_names) {
+                printf("Missing %9s: %s\n", $type, implode(' ', $missing_names));
+            }
+        }
+    }
+    echo "\n\n";
+}
+
+dump_missing_functions();
+dump_missing_classes();
